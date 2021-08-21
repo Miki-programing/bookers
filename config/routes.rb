f1c2354bc43db1a0d9c02b8c554163731d043bd7
Miki-programing <s911m9991@gmail.com>
2021-08-21 04:16:44 +0000

    [Finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # homesに関して
+  root :to => 'homes#homes'
+  # booksに関して
+  resources :books
 end
